f5e99361791c37c2965daf0612aba040975feda4
Alexandria Drake <70923315+AlexandriaDrake@users.noreply.github.com>
2023-02-26 05:59:12 +0000

    Homework 4 Completed

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,37 @@
 //add include statements
+#include "dna.h"
+#include<iostream>
+
+using std::cout; using std::cin; using std::swap;
 
 //add function(s) code here
+
+int factorial(int num)
+{
+    auto factorial = 1;
+
+    while(num>0)
+    {
+        factorial *= num;
+        num--;
+    }
+
+    return factorial;
+}
+
+int gcd(int num1, int num2)
+{
+   while(num1 != num2)
+    {
+        if(num1 < num2)
+        {
+            swap(num2, num1);
+        }
+        else if(num1>num2)
+        {
+            num1 = num1 - num2;
+        }
+    }
+    return num1;
+}
+
