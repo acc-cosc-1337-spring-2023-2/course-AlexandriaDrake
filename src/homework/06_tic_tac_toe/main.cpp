de1c93da50198ea2ca4c629a69d71b279bf198d7
Alexandria Drake <70923315+AlexandriaDrake@users.noreply.github.com>
2023-03-29 05:18:17 +0000

    Homework 6 DRAFT

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,15 @@
+#include<tic_tac_toe.h>
+#include<iostream>
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+    string first_player;
+    cout << "First Player: choose X or O to start the game: ";
+    cin >> first_player;
+    tic_tac_toe game;
+    game.start_game(first_player);
 
-	return 0;
+    return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,75 @@
 //cpp
+
+#include<tic_tac_toe.h>
+#include<iostream>
+#include<string>
+
+using std::cout; using std::cin; using std::endl;
+
+bool tic_tac_toe::game_over()
+{
+    return check_board_full();
+}
+
+void tic_tac_toe::start_game(string first_player)
+{
+    if (first_player == "X" || first_player == "O")
+    {
+        player = first_player;
+        clear_board();
+    }
+    else
+    {
+        cout << "Invalid player selection. Please choose X or O.\n" << endl;
+    }
+}
+
+void tic_tac_toe::mark_board(int position)
+{
+    if (position >= 1 && position <= 9 && pegs[position - 1] == " ")
+    {
+        pegs[position -1] = player;
+        set_next_player();
+    }
+    else
+    {
+        cout << "Invalid position. Please choose an empty position between 1 and 9.\n" << endl;
+    }
+}
+
+string tic_tac_toe::get_player() const
+{
+    return player;
+}
+
+void tic_tac_toe::display_board() const
+{
+    for (int i = 0; i < 9; i +=3)
+    {
+        std::cout << " " << pegs[i] << " | " << pegs [i+2] << " | " << pegs[i+2] << "\n";
+        if (i !=6)
+        {
+            std::cout << "---|---|---\n";
+        }
+    }
+}
+
+void tic_tac_toe::set_next_player()
+{
+    if (player == "X")
+    {
+        player = 'O';
+    }
+    else
+    {
+        player = 'X';
+    }
+}
+
+void tic_tac_toe::clear_board()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        pegs[i] = " ";
+    }
+}
